7fe0d18f18e26ba2a79bf9da35136a8cf7ac9f47
Yonatan Abergel <yonatanab1@gmail.com>
2025-11-26 04:11:22 +0200

    removed old logger and app

--- src/ui/mod.rs
+++
@@ -1,2 +0,0 @@
-﻿pub mod app;
-mod ui;
--- src/universe/logger.rs
+++
@@ -1,7 +0,0 @@
-﻿use crossterm::style::Color;
-
-#[derive(Debug, Clone)]
-pub enum LogMessage {
-    Info(String),
-    UniverseLog { name: String, color: Color, message: String },
-}
